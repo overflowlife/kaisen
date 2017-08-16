58c7f547f7f5bfa63a269dcffb8d601cfc413c05
Y Arake <kno1232006-other3@yahoo.co.jp>
2017-08-17 03:40:44 +0900

    version

--- src/kaisenLib/AppSet.cs
+++ src/kaisenLib/AppSet.cs
@@ -10,7 +10,7 @@ namespace KaisenLib
         static public Encoding enc = Encoding.UTF8;
         static public int defaultPort = 19690;
         static public string loopbackAddress = "127.0.0.1";
-        static public string version = "1.0.0";
+        static public string version = "1.1.0";
         static public char delimiter = ',';
 
         static public void outputArrow(string fmt = "") => System.Console.Write($"{fmt}->");
