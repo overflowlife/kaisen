c7c1d4bf07956cc4a0ef5cd6e16fe16393970834
Y Arake <kno1232006-other3@yahoo.co.jp>
2017-08-29 02:46:16 +0900

    hoho

--- AutoHost01/Program.cs
+++ AutoHost01/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 
-
 namespace AutoHost01
 {
-    /// <summary>
-    /// クライアントの要求に対して海戦ゲームの相手をするサーバプログラムのプロトタイプです。
-    /// </summary>
     class Program
     {
         static void Main(string[] args)
