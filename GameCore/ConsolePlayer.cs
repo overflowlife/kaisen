ed39348ac370607b1ba500b2b98cc05f00b04eb0
dsk <waldo346.gol@gmail.com>
2017-09-05 21:15:13 +0900

    変更

--- GameCore/ConsolePlayer.cs
+++ GameCore/ConsolePlayer.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 namespace GameCore
 {
     /// <summary>
-    /// コンソールからの入力によりゲームを進行するプレイヤーです。
+    /// コンソールからの入力によりゲームを進行するプレイヤーです。kopi-
     /// </summary>
     public class ConsolePlayer : IPlayer
     {
