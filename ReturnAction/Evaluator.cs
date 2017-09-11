aa6db4a1e82cadbdb6cc81e5e33e152de0d3e6cd
Y Arake <kno1232006-other3@yahoo.co.jp>
2017-09-12 07:07:28 +0900

    a

--- ReturnAction/Evaluator.cs
+++ ReturnAction/Evaluator.cs
@@ -2,8 +2,12 @@
 
 namespace ReturnAction
 {
-
+    /// <summary>
+    /// ある盤面時点での評価値を生成するメソッドを持つクラスの基礎クラスです。
+    /// </summary>
     public abstract class Evaluator
     {
+       
+            
     }
 }
